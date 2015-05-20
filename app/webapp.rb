1065aba496e22124acf02a1c172ed6b5a938a481
JP Hastings-Spital <jphastings@gmail.com>
2015-05-20 14:50:04 +0100

    Demo branch

--- app/webapp.rb
+++ app/webapp.rb
@@ -3,7 +3,7 @@ require "sinatra/base"
 module Branchinator
   class WebApp < Sinatra::Base
     get "/" do
-      "This is the branchinator"
+      "This is the branchinator - demo branch"
     end
   end
 end
